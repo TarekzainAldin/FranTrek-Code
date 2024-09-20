e2d36b695d81a6301f656b24f59c3fb13fa602c8
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-09-20 10:26:39 +0200

    disign the home page and make and bring the json file for the courses and the lesone

--- app.py
+++ app.py
@@ -2,37 +2,85 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-lessons=[{
-    
-        'title':'request Libray course',
-        'course':'python',
-        'authour':'tarek'
-   
+lessons = [{
+    'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'tarek',
+    'thumbnail': 'thumbnail.jpg'
 },
-{
-    
-        'title':'request html course',
-        'course':'Html',
-        'authour':'francia'
-   
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'tarek',
+    'thumbnail': 'thumbnail.jpg'
 },
-
-{
-    
-        'title':'request datatime course',
-        'course':'python',
-        'authour':'francia  '
-   
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'francia',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'francia',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'francia',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'tarek',
+    'thumbnail': 'thumbnail.jpg'
 },
 ]
 
-@app.route('/')
-@app.route('/home')
+courses = [
+{
+        'name': 'Python',
+        'icon': 'python.svg',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+    {
+        'name': 'Data Analysis',
+        'icon': 'analysis.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+    {
+        'name': 'Machine Learning',
+        'icon': 'machine-learning.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Web Design',
+        'icon': 'web.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Blockchain',
+        'icon': 'blockchain.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Tips & Tricks',
+        'icon': 'idea.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+]
+@app.route("/")
+@app.route("/home")
 def home():
-    return render_template('home.html', lessons=lessons)
-@app.route('/about')
+    return render_template('home.html', lessons=lessons, courses=courses)
+
+@app.route("/about")
 def about():
-    return render_template('about.html', title = "About")
-if __name__ == '__main__':
+    return render_template('about.html', title="About")
+
+if __name__=="__main__":
     app.run(debug=True)
-    
