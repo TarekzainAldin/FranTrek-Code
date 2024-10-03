0b2580eeb1fc363d48a7cbb61f2b931b14936f4b
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-10-03 10:13:42 +0200

    install flask login manger

--- FranTrek/models.py
+++ FranTrek/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from FranTrek import db
+from FranTrek import db , logoin_manger
+from flask_login import UserMixin
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
