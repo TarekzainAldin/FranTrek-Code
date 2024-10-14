db6e12a032acd84f7af4d96a5bde2ad5781117d8
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-10-14 15:26:00 +0200

    install all the backege and stay the error makup

--- FranTrek/routes.py
+++ FranTrek/routes.py
@@ -94,9 +94,24 @@ def save_picture(form_picture):
     return picture_name
 
 
+def get_previous_next_lesson(lesson):
+    course = lesson.course_name
+    for lsn in course.lessons:
+        if lsn.title == lesson.title:
+            index = course.lessons.index(lsn)
+            previous_lesson = course.lessons[index - 1] if index > 0 else None
+            next_lesson = (
+                course.lessons[index + 1] if index < len(course.lessons) - 1 else None
+            )
+            break
+    return previous_lesson, next_lesson
+
+
 @app.route("/")
 @app.route("/home")
 def home():
+    lessons = Lesson.query.all()
+    courses = Course.query.all()
     return render_template("home.html", lessons=lessons, courses=courses)
 
 
@@ -183,9 +198,6 @@ def profile():
         image_file=image_file,
         active_tab="profile",
     )
-
-
-
 
 
 @app.route("/dashboard/new_lesson", methods=["GET", "POST"])
