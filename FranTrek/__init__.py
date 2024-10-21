153773fb10ff8e5a9414bce01a3bd9f5a1363331
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-10-21 13:27:06 +0200

    adding the ckeditor and justfy and code snape on the course andlessons and testing witout any wrong

--- FranTrek/__init__.py
+++ FranTrek/__init__.py
@@ -12,6 +12,7 @@ app.config[
 ] = '16c749724f0c2920edea51b1b4e7af80f9a10f2ed97981830c0c9109f2370862'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///FranTrek.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config['CKEDITOR_ENABLE_CODESNIPPET'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
